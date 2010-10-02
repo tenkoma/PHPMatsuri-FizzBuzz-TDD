76a767f53efed2090292400f0254151da77a5190
tenkoma <tenkoma@gmail.com>
2010-10-03 00:50:10 +0900

    FizzBuzz class Not Found!

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,6 +1,10 @@
 <?php
 require_once 'FizzBuzz.php';
 class FizzBuzzTest extends PHPUnit_Framework_TestCase {
+    public $object;
+    public function setUp(){
+        $this->object = new FizzBuzz();
+    }
     public function testOne(){
     }
 }
